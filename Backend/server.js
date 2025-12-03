bc348e29b9e62f6e3b816bfeaa962fc08499fc99
engineerswelfaretrust-lgtm <engineerswelfaretrust@gmail.com>
2025-12-03 16:14:13 +0530

    update cors origin

--- Backend/server.js
+++ Backend/server.js
@@ -16,8 +16,8 @@ const allowedOrigins = [
   process.env.VITE_FRONTEND_URL, // optional alternative env var
   "http://localhost:5173", // vite dev server origin
   "http://localhost:5000",
-  "https://erswelfareindia-phi.vercel.app",
-  "https://www.engwelfare.com", 
+  "https://engineerwelfare-yc2s.vercel.app/",
+  "https://www.engwelfare.com",
 ].filter(Boolean);
 
 const corsOptions = {
